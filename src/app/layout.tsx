fa16aba2dcb2de7bbbcd33f1e2d97de84c00c6f6
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-07-06 13:38:03 -0700

    replace DM Sans font with Lato in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,49 +1,49 @@
 import QueryProvider from "@/components/providers/QueryProvider";
 import "./globals.css";
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
+import { Lato } from "next/font/google";
 
-const dm_sans = DM_Sans({
-  subsets: ["latin"],
-  variable: "--font-dm_sans",
-  display: "swap",
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+const lato = Lato({
+	subsets: ["latin"],
+	variable: "--font-lato",
+	display: "swap",
+	weight: ["100", "300", "400", "700", "900"],
 });
 
 export const metadata: Metadata = {
-  title: {
-    default: "NACOS",
-    template: "%s | NACOS",
-  },
-  description: "Welcome to NACOS.",
-  openGraph: {
-    title: "NACOS",
-    description: "Welcome to NACOS.",
-    url: "https://nacos.example.com",
-    siteName: "NACOS",
-    images: [
-      {
-        url: "https://nacos.example.com/og-image.png",
-        width: 1200,
-        height: 630,
-        alt: "NACOS Open Graph Image",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
+	title: {
+		default: "NACOS",
+		template: "%s | NACOS",
+	},
+	description: "Welcome to NACOS.",
+	openGraph: {
+		title: "NACOS",
+		description: "Welcome to NACOS.",
+		url: "https://nacos.example.com",
+		siteName: "NACOS",
+		images: [
+			{
+				url: "https://nacos.example.com/og-image.png",
+				width: 1200,
+				height: 630,
+				alt: "NACOS Open Graph Image",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${dm_sans.className} antialiased overflow-y-auto`}>
-        <QueryProvider>{children}</QueryProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${lato.className} antialiased overflow-y-auto`}>
+				<QueryProvider>{children}</QueryProvider>
+			</body>
+		</html>
+	);
 }
